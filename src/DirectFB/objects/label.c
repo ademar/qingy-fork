de4c3a7555e05957d104e697ba3fb930d66ceb01
noberasco <noberasco@049dde28-9910-0410-bbc7-be8585b37828>
2003-12-23 22:30:17 +0000

    blurb
    git-svn-id: https://qingy.svn.sourceforge.net/svnroot/qingy/trunk/qingy@159 049dde28-9910-0410-bbc7-be8585b37828

--- src/DirectFB/objects/label.c
+++ src/DirectFB/objects/label.c
@@ -63,20 +63,15 @@ void Plot(Label *thiz)
 void Label_ClearText(Label *thiz)
 {
 	if (!thiz) return;
-	if (thiz->text)
-	{
-		free(thiz->text);
-		thiz->text = NULL;
-	}
+	free(thiz->text);
 	Plot(thiz);
 }
 
 void Label_SetText(Label *thiz, char *text, int alignment)
 {
 	if (!thiz || !text) return;
-	if (thiz->text) Label_ClearText(thiz);
-	thiz->text = (char *) calloc(strlen(text)+1, sizeof(char));
-	strcpy(thiz->text, text);
+	free(thiz->text);
+	thiz->text = strdup(text);
 	thiz->alignment = alignment;
 	Plot(thiz);
 }
