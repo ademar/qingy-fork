e86a7c170a5ea2d6c2c173186a764310228f8718
pa0lino <pa0lino@049dde28-9910-0410-bbc7-be8585b37828>
2004-01-26 23:42:39 +0000

    going on making the matrix screensaver
    git-svn-id: https://qingy.svn.sourceforge.net/svnroot/qingy/trunk/qingy@215 049dde28-9910-0410-bbc7-be8585b37828

--- media/screensavers/matrix/matrix.c
+++ media/screensavers/matrix/matrix.c
@@ -31,7 +31,7 @@
 #include <screensaver_module.h>
 
 #define COLS 80
-#define ROWS 10
+#define ROWS 24
 void 
 screen_saver_entry(Q_screen_t env)
 {
@@ -39,19 +39,27 @@ screen_saver_entry(Q_screen_t env)
   int posx;
   int posy;
   unsigned int seconds=0;
-  unsigned int milli_seconds=50;
+  unsigned int milli_seconds=100;
   int i, j;
   int cel_w, cel_h;
   
   char Matrix[ROWS][COLS];
-  char *firstone=Matrix[0];
- 
+  char speeds[COLS];
+  char curspeed[COLS];
+  IDirectFBFont *font;
+  DFBFontDescription font_dsc;
+  
 
-   /* initialize matrix to 0 */
-  for(i=1; i<ROWS; i++){
-    for(j=0; j<COLS; j++){
-      Matrix[i][j]=0;
+  /* initialize matrix to 0 */
+  for(i=1; i<COLS; i++){
+    for(j=0; j<ROWS; j++){
+      Matrix[j][i]=0;
     }
+  }
+  
+  for(i=0; i<COLS; i++){
+    speeds[i]=rand()%100;
+    curspeed[i]=0;
   }
   /* our rect dims: */
   cel_w=env.screen_width / COLS;
@@ -61,28 +69,45 @@ screen_saver_entry(Q_screen_t env)
   /* we clear event buffer to avoid being bailed out immediately */
   env.screen_saver_events->Reset(env.screen_saver_events);
   
-   
+  
   /* do screen saver until an input event arrives */
   while (1)
     {
       for(i=0; i<COLS; i++){
-	if(!rand()%4) firstone[i]='\0';
-	firstone[i]=(rand()%(127-32))+32;
+	if(!rand()%2) 
+	  Matrix[0][i]='\0';
+	else 
+	  Matrix[0][i]=(rand()%(127-32))+32;
       }
+      font_dsc.flags = DFDESC_HEIGHT;
+      font_dsc.height = 22;
+      env.dfb->CreateFont(env.dfb, "/etc/qingy/screensavers/matr.ttf", &font_dsc, &font);
+      env.surface->SetFont(env.surface, font);
       env.surface->Clear (env.surface, 0x00, 0x00, 0x00, 0xFF);
-      env.surface->SetColor (env.surface, 0xFF, 0x50, 0x50, 0xFF);
-      for(i=0; i<ROWS; i++){
-	for(j=0; j<COLS; j++){
-	  if(*(firstone+(i*COLS)+j))
+      env.surface->SetColor (env.surface, 0x40, 0xAF, 0x40, 0xAA);
+      for(i=0; i<COLS; i++){
+	if(speeds[i] > curspeed[i])
+	  {
+	    curspeed[i]++;
+	    continue;
+	  }
+	else{
+	  curspeed[i]=0;
+	  speeds[i]=rand()%100;
+	}
+	for(j=0; j<ROWS; j++){
+	  if(Matrix[j][i])
 	    env.surface->DrawGlyph (env.surface,
-				    *(firstone+(i*COLS)+j)
-				    , env.screen_width - j*cel_h, env.screen_height - i*cel_w, DSTF_LEFT);
+				    Matrix[j][i],
+				    i* cel_h,  j * 3 *cel_w, DSTF_LEFT);
 	}
       }
       env.surface->Flip (env.surface, NULL, DSFLIP_BLIT);
-      firstone+=COLS;
-      firstone = (firstone >= &Matrix[ROWS][COLS]? Matrix[0]:firstone);
-      
+      for(i=ROWS-1; i; i--){
+	for(j=0; j<COLS; j++){
+	  Matrix[i][j]=Matrix[i-1][j];
+	}
+      }
       env.screen_saver_events->WaitForEventWithTimeout(env.screen_saver_events, seconds, milli_seconds);
       if (env.screen_saver_events->HasEvent(env.screen_saver_events) == DFB_OK)
 	break;
